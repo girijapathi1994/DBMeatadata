fcdc8c0d2c54e3177f5c9318f0d74a2916432233
Anudeep <anudeep.uidev@gmail.com>
2019-05-19 15:30:02 -0500

    added for change

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component,OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-  title = 'DB';
+  title = 'DB Metadata';
   constructor(){
 
   }
