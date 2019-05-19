2022abaeda6543f79a68954bef6cf46eb2675c62
Anudeep <anudeep.uidev@gmail.com>
2019-05-19 16:35:09 -0500

    gggg

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -7,11 +7,7 @@ import { Component,OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-<<<<<<< HEAD
   title = 'DB Metadata dev new updated new new';
-=======
-  title = 'DB Metadata dev new new';
->>>>>>> c5e79e62ad05ba323cf5ca2555a50718248416d4
   constructor(){
 
   }
