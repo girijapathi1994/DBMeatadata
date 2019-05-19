0ab281bd44eeafecc53de7d081a0eeb6662dbaa4
Anudeep <anudeep.uidev@gmail.com>
2019-05-19 15:36:35 -0500

    aaded for new dev branch

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component,OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-  title = 'DB Metadata';
+  title = 'DB Metadata dev new';
   constructor(){
 
   }
